16870ca6687283a0eb7d2702708ef454db7a3860
Avinash Kr Mandal <avinashkrmandal03@gmail.com>
2025-11-20 12:09:04 +0530

    course controller testing

--- backend/src/routes/course.routes.js
+++ backend/src/routes/course.routes.js
@@ -1,6 +1,15 @@
-import { Router } from 'express';
-import { authMiddleware } from '../middlewares/auth.middleware.js';
-import { createCourse, deleteCourse, enrollCourse, getCourseById, listCourses, publishCourse, updateCourse } from '../controllers/course.controller.js';
+import { Router } from "express";
+import {
+  createCourse,
+  deleteCourse,
+  enrollCourse,
+  getCourseById,
+  listCourses,
+  publishCourse,
+  updateCourse,
+} from "../controllers/course.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/role.middleware.js";
 
 const router = Router();
 
@@ -9,13 +18,32 @@ router.get("/", listCourses);
 router.get("/:id", getCourseById);
 
 // instructor action
-router.post("/", authMiddleware, authorizeRoles("instructor","admin"), createCourse);
-router.put("/:id", authMiddleware, authorizeRoles("instructor","admin"), updateCourse);
-router.delete("/:id", authMiddleware, authorizeRoles("instructor","admin"), deleteCourse);
-router.post("/:id/publish", authMiddleware, authorizeRoles("instructor","admin"), publishCourse);
+router.post(
+  "/",
+  authMiddleware,
+  authorizeRoles("instructor", "admin"),
+  createCourse
+);
+router.put(
+  "/:id",
+  authMiddleware,
+  authorizeRoles("instructor", "admin"),
+  updateCourse
+);
+router.delete(
+  "/:id",
+  authMiddleware,
+  authorizeRoles("instructor", "admin"),
+  deleteCourse
+);
+router.post(
+  "/:id/publish",
+  authMiddleware,
+  authorizeRoles("instructor", "admin"),
+  publishCourse
+);
 
 // enrollment/payment
 router.post("/:id/enroll", authMiddleware, enrollCourse);
 
 export default router;
-
